9773c10d42371cd69123cc6c4ffa73425fd5a5ad
claudiaastefanelli <stefanelli.claudia@iisgalvanimi.edu.it>
2022-11-04 10:41:41 +0000

    versione1

--- es1.py
+++ es1.py
@@ -51,4 +51,108 @@ myVariableName = "John"
 MyVariableName = "John"
 my_variable_name = "John"
 
+print("###########")
+#posso assegnare valori a più variabili in una riga:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+print("#########")
+#posso assegnare lo stesso valore a più variabili in una riga:
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+print("########")
+#Se ho una raccolta di valori in un elenco, posso estrarre i valori in variabili (spacchettamento).
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+print("############")
+#print()viene utilizzata per generare variabili.
+x = "Python is awesome"
+print(x)
+
+print("########")
+#in print()funzione, emetti più variabili, separate da una virgola
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+print("########")
+#metto lo spazio dopo python e is altrimenti stamperebbe la frase attaccata
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+print("############")
+#Per i numeri, il +carattere funziona come un operatore matematico
+x = 5
+y = 10
+print(x + y)
+
+print("##########")
+#in print(), quando provi a combinare una stringa e un numero con l' + operatore, Python ti darà un errore
+
+print("###########")
+#per generare più variabili in print() bisogna separarle con virgole, che supportano anche diversi tipi di dati
+x = 5
+y = "John"
+print(x, y)
+
+print ("###########")
+#Creo una variabile al di fuori di una funzione e la uso all'interno della funzione
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+print("##########")
+#Creo una variabile all'interno di una funzione, con lo stesso nome della variabile globale
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+print("###########")
+#Se si utilizza la globalparola chiave, la variabile appartiene all'ambito globale
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+print("###########")
+#Per modificare il valore di una variabile globale all'interno di una funzione, fare riferimento alla variabile usando la globalparola chiave
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+print("#########")
+#le liste vengono create per memorizzare più elementi in una singola variabile
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
 
--- umberto.py
+++ umberto.py
@@ -1,1 +1,29 @@
-print("ciao umberto")
+#se devo scrivere una variabile nome uso str, per numero intero int e per numero decimale float
+x=str("coniglio")
+y=str("pecora")
+z=str("cavallo")
+c=str("leone")
+a=str("criceto")
+
+print(x)
+print(y)
+print(z)
+print(c)
+print(a)
+
+#se devo stampare una condizione di maggiore o minore metto if con la condizione e metto else con l'altra condizione
+x=3
+y=5
+
+if 3>=5:
+print ("ciao")
+
+else print("arrivederci")
+
+#per stampare una lista uso la parentesi quadra,, inserisco i nomi e poi stampo (se ci sono due uguali li considera diversi)
+thislist= ["apple","banana","cherry"]
+print (thislist)
+
+#se invece voglio sapere quanti sono i nomi che sto stampando uso la funzione len dopo aver scritto il print
+thislist= ["apple,"banana"]
+print(len(thislist))
